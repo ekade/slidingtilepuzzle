99738bd15384448eb3ff38daf74ef5fb99f41c1c
ekade <evadelnuevo@gmail.com>
2017-03-31 14:10:09 +0200

    First commit

---
+++ SlidingTilePuzzle/SlidingTilePuzzle/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace SlidingTilePuzzle
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.tbLayout.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbLayout
+            // 
+            this.tbLayout.ColumnCount = 3;
+            this.tbLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tbLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tbLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tbLayout.Controls.Add(this.panel1, 0, 0);
+            this.tbLayout.Controls.Add(this.panel2, 1, 0);
+            this.tbLayout.Controls.Add(this.panel3, 2, 0);
+            this.tbLayout.Controls.Add(this.panel4, 0, 1);
+            this.tbLayout.Controls.Add(this.panel5, 2, 1);
+            this.tbLayout.Controls.Add(this.panel6, 0, 2);
+            this.tbLayout.Controls.Add(this.panel7, 1, 2);
+            this.tbLayout.Controls.Add(this.panel8, 2, 2);
+            this.tbLayout.Controls.Add(this.panel9, 1, 1);
+            this.tbLayout.Location = new System.Drawing.Point(12, 12);
+            this.tbLayout.Name = "tbLayout";
+            this.tbLayout.RowCount = 3;
+            this.tbLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tbLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tbLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tbLayout.Size = new System.Drawing.Size(260, 238);
+            this.tbLayout.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 73);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 73);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button3.Location = new System.Drawing.Point(-1, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 73);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button4.Location = new System.Drawing.Point(0, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 73);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button5.Location = new System.Drawing.Point(0, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 73);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button6.Location = new System.Drawing.Point(0, 0);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(80, 73);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Silver;
+            this.button7.Location = new System.Drawing.Point(0, 1);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(80, 73);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.White;
+            this.button8.Location = new System.Drawing.Point(0, 0);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(80, 73);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(80, 73);
+            this.panel1.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(89, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(80, 73);
+            this.panel2.TabIndex = 9;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Location = new System.Drawing.Point(175, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(82, 73);
+            this.panel3.TabIndex = 10;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel4.Controls.Add(this.button4);
+            this.panel4.Location = new System.Drawing.Point(3, 82);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(80, 73);
+            this.panel4.TabIndex = 11;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel5.Controls.Add(this.button5);
+            this.panel5.Location = new System.Drawing.Point(175, 82);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(82, 73);
+            this.panel5.TabIndex = 12;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.panel6.Controls.Add(this.button6);
+            this.panel6.Location = new System.Drawing.Point(3, 161);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(80, 74);
+            this.panel6.TabIndex = 13;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Silver;
+            this.panel7.Controls.Add(this.button7);
+            this.panel7.Location = new System.Drawing.Point(89, 161);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(80, 74);
+            this.panel7.TabIndex = 14;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.White;
+            this.panel8.Controls.Add(this.button8);
+            this.panel8.Location = new System.Drawing.Point(175, 161);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(82, 74);
+            this.panel8.TabIndex = 15;
+            // 
+            // panel9
+            // 
+            this.panel9.Location = new System.Drawing.Point(89, 82);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(80, 73);
+            this.panel9.TabIndex = 16;
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(15, 257);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(75, 23);
+            this.btnRandom.TabIndex = 1;
+            this.btnRandom.Text = "Random";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(190, 256);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 292);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.tbLayout);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sliding Tile Puzzle";
+            this.tbLayout.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel8.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tbLayout;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ SlidingTilePuzzle/SlidingTilePuzzle/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SlidingTilePuzzle
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        #region Events
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            MoveTile(button);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            MoveTile(button);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            MoveTile(button);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            MoveTile(button);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            MoveTile(button);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            MoveTile(button);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            MoveTile(button);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            MoveTile(button);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                button1.Parent = tbLayout.GetControlFromPosition(0, 0);
+                button2.Parent = tbLayout.GetControlFromPosition(1, 0);
+                button3.Parent = tbLayout.GetControlFromPosition(2, 0);
+
+                button4.Parent = tbLayout.GetControlFromPosition(0, 1);
+                button5.Parent = tbLayout.GetControlFromPosition(2, 1);
+
+                button6.Parent = tbLayout.GetControlFromPosition(0, 2);
+                button7.Parent = tbLayout.GetControlFromPosition(1, 2);
+                button8.Parent = tbLayout.GetControlFromPosition(2, 2);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var usedNumbers = new List<int>();
+                int number = GetRandomNumber(usedNumbers);
+
+                Controls.Find("button" + number, true).First().Parent = tbLayout.GetControlFromPosition(0, 0);
+                usedNumbers.Add(number);
+                number = GetRandomNumber(usedNumbers);
+
+                Controls.Find("button" + number, true).First().Parent = tbLayout.GetControlFromPosition(1, 0);
+                usedNumbers.Add(number);
+                number = GetRandomNumber(usedNumbers);
+
+                Controls.Find("button" + number, true).First().Parent = tbLayout.GetControlFromPosition(2, 0);
+                usedNumbers.Add(number);
+                number = GetRandomNumber(usedNumbers);
+
+                Controls.Find("button" + number, true).First().Parent = tbLayout.GetControlFromPosition(0, 1);
+                usedNumbers.Add(number);
+                number = GetRandomNumber(usedNumbers);
+
+                Controls.Find("button" + number, true).First().Parent = tbLayout.GetControlFromPosition(2, 1);
+                usedNumbers.Add(number);
+                number = GetRandomNumber(usedNumbers);
+
+                Controls.Find("button" + number, true).First().Parent = tbLayout.GetControlFromPosition(0, 2);
+                usedNumbers.Add(number);
+                number = GetRandomNumber(usedNumbers);
+
+                Controls.Find("button" + number, true).First().Parent = tbLayout.GetControlFromPosition(1, 2);
+                usedNumbers.Add(number);
+                number = GetRandomNumber(usedNumbers);
+
+                Controls.Find("button" + number, true).First().Parent = tbLayout.GetControlFromPosition(2, 2);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void MoveTile(Button tile)
+        {
+            try
+            {
+                var tilePosition = tbLayout.GetCellPosition(tile.Parent);
+
+                if (tilePosition.Row + 1 < tbLayout.RowCount && IsThereGap(tilePosition.Row + 1, tilePosition.Column))
+                    tile.Parent = tbLayout.GetControlFromPosition(tilePosition.Column, tilePosition.Row + 1);
+
+                if (tilePosition.Row - 1 >= 0 && IsThereGap(tilePosition.Row - 1, tilePosition.Column))
+                    tile.Parent = tbLayout.GetControlFromPosition(tilePosition.Column, tilePosition.Row - 1);
+
+                if (tilePosition.Column - 1 >= 0 && IsThereGap(tilePosition.Row, tilePosition.Column - 1))
+                    tile.Parent = tbLayout.GetControlFromPosition(tilePosition.Column - 1, tilePosition.Row);
+
+                if (tilePosition.Column + 1 < tbLayout.ColumnCount && IsThereGap(tilePosition.Row, tilePosition.Column + 1))
+                    tile.Parent = tbLayout.GetControlFromPosition(tilePosition.Column + 1, tilePosition.Row);
+
+                tile.BringToFront();
+                tbLayout.Update();
+
+                if (GameWon())
+                    MessageBox.Show("¡¡ Conseguido !!", "Fin");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Movimiento no permitido");
+            }
+        }
+
+        private bool IsThereGap(int row, int column)
+        {
+            var control = tbLayout.GetControlFromPosition(column, row);
+            return control.Controls.Count == 0;
+        }
+
+        private bool GameWon()
+        {
+            var gameWon = button1.Parent == tbLayout.GetControlFromPosition(0, 0)
+                && button2.Parent == tbLayout.GetControlFromPosition(1, 0)
+                && button3.Parent == tbLayout.GetControlFromPosition(2, 0)
+
+                && button4.Parent == tbLayout.GetControlFromPosition(0, 1)
+                && button5.Parent == tbLayout.GetControlFromPosition(2, 1)
+
+                && button6.Parent == tbLayout.GetControlFromPosition(0, 2)
+                && button7.Parent == tbLayout.GetControlFromPosition(1, 2)
+                && button8.Parent == tbLayout.GetControlFromPosition(2, 2);
+
+            return gameWon;
+        }
+
+        private int GetRandomNumber(List<int> excludedNumbers)
+        {
+            Random rnd = new Random();
+
+            int number = rnd.Next(1, 9);
+
+            while (excludedNumbers.Contains(number))
+                number = rnd.Next(1, 9);
+
+            return number;
+        }
+
+        #endregion
+    }
+}
